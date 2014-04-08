62302fa21db64f05aff7f931cbd7fa020a6eca5e
Hua Zhou <hua_zhou@ncsu.edu>
2014-04-08 21:28:10 +0000

    path following from rho=0 working now

--- demo_classopath.m
+++ demo_classopath.m
@@ -6,8 +6,8 @@ s = RandStream('mt19937ar','Seed',1);
 RandStream.setGlobalStream(s);
 
 % dimension
-n = 100;
-p = 20;
+n = 1000;
+p = 200;
 
 % truth with sum constraint sum(b)=0
 beta = zeros(p,1);
@@ -24,12 +24,65 @@ beq = 0;
 X = randn(n,p);
 y = X*beta + randn(n,1);
 
-% obtain solution path
+%% obtain solution path by path following
 tic;
-[rhopath,betapath,dualpathEq,dualpathIneq] ...
-    = lsq_classopath(X,y,[],[],Aeq,beq,'qp_solver','GUROBI');
-toc;
-
+[rhopath,betapath] ...
+    = lsq_classopath(X,y,[],[],Aeq,beq,'qp_solver','gurobi');
+timing_path = toc;
 
 % plot solutions
-%figure; hold on;
+figure; hold on;
+set(gca,'FontSize', 20);
+plot(rhopath, betapath');
+xlabel('\rho');
+ylabel('\beta(\rho)');
+xlim([min(rhopath) max(rhopath)*1.05]);
+title(['path following algorithm:' num2str(timing_path) ' s']);
+
+%% obtain solution path by GUROBI optimization at grid
+
+betapath_gurobi = zeros(size(betapath));
+tic;
+for k = 1:length(rhopath)
+    display(k);
+    [betapath_gurobi(:,k)] ...
+    = lsq_constrsparsereg(X,y,rhopath(k),...
+    'method','qp','qp_solver','matlab','Aeq', Aeq, 'beq', beq);
+end
+timing_gurobi = toc;
+
+% plot solutions
+figure; hold on;
+set(gca,'FontSize', 20);
+plot(rhopath, betapath_gurobi');
+xlabel('\rho');
+ylabel('\beta(\rho)');
+xlim([min(rhopath) max(rhopath)*1.05]);
+title(['Gurobi on grid:' num2str(timing_gurobi) ' s']);
+
+%% obtain solution path by ADMM optimization at grid
+
+betapath_admm = zeros(size(betapath));
+tic;
+for k = 1:length(rhopath)
+    display(k);
+    
+    if k==1
+        x0 = zeros(p,1);
+    else
+        x0 = betapath(:,k-1);
+    end
+    [betapath_admm(:,k)] ...
+        = lsq_constrsparsereg(X,y,rhopath(k),...
+        'method','admm','projC', @(x) x-mean(x),'x0',x0);
+end
+timing_admm = toc;
+
+% plot solutions
+figure; hold on;
+set(gca,'FontSize', 20);
+plot(rhopath, betapath_admm');
+xlabel('\rho');
+ylabel('\beta(\rho)');
+xlim([min(rhopath) max(rhopath)*1.05]);
+title(['ADMM on grid:' num2str(timing_admm) ' s']);
--- lsq_classopath.m
+++ lsq_classopath.m
@@ -80,29 +80,34 @@ dualpathEq = zeros(m1, maxiters);
 dualpathIneq = zeros(m2, maxiters);
 rhopath = zeros(1, maxiters);
 
-% intialize beta by linear programming
+% intialize beta by quadratic programming
+H = X'*X;
 if strcmpi(qp_solver, 'matlab')
-    % use Matlab linprog
-    [x,~,~,~,lambda] = ...
-        linprog(ones(2*p,1),[A -A],b,[Aeq -Aeq],beq, ...
-        zeros(2*p,1), inf(2*p,1));
-    betapath(:,1) = x(1:p) - x(p+1:end);
+    % use Matlab lsqlin
+    options.Algorithm = 'interior-point-convex';
+    options.Display = 'off';
+    [x,~,~,~,~,lambda] = lsqlin(X,y,A,b,Aeq,beq,[],[],[],options);
+    betapath(:,1) = x;
     dualpathEq(:,1) = lambda.eqlin;
     dualpathIneq(:,1) = lambda.ineqlin;
 elseif strcmpi(qp_solver, 'GUROBI')
     % use QUROBI solver if possible
-    gmodel.obj = ones(2*p,1);
-    gmodel.A = sparse([A -A; Aeq -Aeq]);
-    gmodel.sense = [repmat('<', m1, 1); repmat('=', m2, 1)];
-    gmodel.rhs = [b; beq];
-    gmodel.lb = zeros(2*p,1);
+    gmodel.obj = - X'*y;
+    gmodel.A = sparse([Aeq; A]);
+    gmodel.sense = [repmat('=', m1, 1); repmat('<', m2, 1)];
+    gmodel.rhs = [beq; b];
+    gmodel.Q = sparse(H)/2;
+    gmodel.lb = - inf(p,1);
+    gmodel.ub = inf(p,1);
+    gmodel.objcon = norm(y)^2/2;
     gparam.OutputFlag = 0;
     gresult = gurobi(gmodel, gparam);
-    betapath(:,1) = gresult.x(1:p) - gresult.x(p+1:end);
+    betapath(:,1) = gresult.x;
     dualpathEq(:,1) = gresult.pi(1:m1);
     dualpathIneq(:,1) = reshape(gresult.pi(m1+1:end),m2,1);
 end
 setActive = abs(betapath(:,1))>1e-16;
+nActive = nnz(setActive);
 betapath(~setActive,1) = 0;
 setIneqBorder = dualpathIneq(:,1)>0;
 residIneq = A*betapath(:,1) - b;
@@ -112,76 +117,88 @@ resid = y - X(:,setActive)*betapath(setActive,1);
 grad = X(:,~setActive)'*resid ...
     - Aeq(:,~setActive)'*dualpathEq(:,1) ...
     - A(:,~setActive)'*dualpathIneq(:,1);
-rhopath(1) = max(abs(grad));
+rhopath(1) = 0;
 
 % intialize subgradient vector
 subgrad = zeros(p,1);
-subgrad(betapath(:,1)>0) = 1;
-subgrad(betapath(:,1)<0) = -1;
+subgrad(setActive) = sign(betapath(setActive,1));
 subgrad(~setActive) = grad / rhopath(1);
 setActive = abs(subgrad-1)<1e-8 | abs(subgrad+1)<1e-8;
 
 % main loop for path following
-H = X'*X;
 for k = 2:maxiters
     
     % path following direction
     M = [H(setActive, setActive) Aeq(:,setActive)' A(:,setActive)'];
-    M(end+1:end+m1+nnz(setIneqBorder), 1:nnz(setActive)) = ...
+    M(end+1:end+m1+nnz(setIneqBorder), 1:nActive) = ...
         [Aeq(:,setActive); A(:,setActive)];
-    dir = M \ [subgrad(setActive); zeros(m1+nnz(setIneqBorder,1))];
+    dir = - M \ [subgrad(setActive); zeros(m1+nnz(setIneqBorder),1)];
     dirSubgrad = ...
-        - [H(~setActive, setActive), Aeq(:,~setActive)' A(:,~setActive)']...
+        - [H(~setActive, setActive) Aeq(:,~setActive)' A(:,~setActive)']...
         * dir;
-    dirResidIneq = A(~setIneqBorder,~setActive)'*dir(1:nnz(setActive));
+    dirResidIneq = A(~setIneqBorder,setActive)*dir(1:nActive);
+
+    % terminate path following
+    if max(abs(dir(1:nActive)))<1e-16
+        break;
+    end    
     
     % next rho for beta
     nextrhoBeta = inf(p, 1);
     nextrhoBeta(setActive) = - betapath(setActive,k-1) ...
-        ./ dir(1:nnz(setActive));
-    nextrhoBeta(~setActive) = max( ...
-        rhopath(:,k-1)*(subgrad(~setActive)+1)./(-dirSubgrad+1), ...
-        rhopath(:,k-1)*(subgrad(~setActive)-1)./(-dirSubgrad-1) );
-    nextrhoBeta(nextrhoBeta<0) = inf;
+        ./ dir(1:nActive);
+    nextrhoBeta(~setActive) = ...
+        rhopath(:,k-1) * (sign(dirSubgrad)-subgrad(~setActive)) ...
+        ./(dirSubgrad-sign(dirSubgrad));
+    nextrhoBeta(nextrhoBeta<=1e-8) = inf;
     
     % next rho for inequality constraints
     nextrhoIneq = inf(m2, 1);
     nextrhoIneq(setIneqBorder) = - dualpathIneq(setIneqBorder,k-1) ...
-        ./ dir(end-nnz(setIneqBorder)+1,end);
+        ./ dir(nActive+m1+1:end);
     nextrhoIneq(~setIneqBorder) = - residIneq ./ dirResidIneq;
     nextrhoIneq(nextrhoIneq<0) = inf;
     
     % determine next rho
-    [chgrho, idx] = min([nextrhoBeta; nextrhoIneq]);
+    [chgrho,idx] = min([nextrhoBeta; nextrhoIneq]);
     
     % move to next rho
-    rhopath(k) = rhopath(k-1) - chgrho;
+    rhopath(k) = rhopath(k-1) + chgrho;
     betapath(setActive,k) = betapath(setActive,k-1) ...
-        + chgrho*dir(1:nnz(setActive));
+        + chgrho*dir(1:nActive);
     dualpathEq(:,k) = dualpathEq(:,k-1) ...
-        + chgrho*dir(nnz(setActive)+1:nnz(setActive)+m1);
+        + chgrho*dir(nActive+1:nActive+m1);
     dualpathIneq(setIneqBorder,k) = dualpathIneq(setIneqBorder,k-1) ...
-        + chgrho*dir(nnz(setActive)+m1+1:end);
+        + chgrho*dir(nActive+m1+1:end);
     subgrad(~setActive) = ...
-        (rhopath(k-1)*subgrad(~setActive) + chgtol*dirSubgrad)/rhopath(k);
+        (rhopath(k-1)*subgrad(~setActive) + chgrho*dirSubgrad)/rhopath(k);
     residIneq = A*betapath(:,k) - b;
     
     % update sets
+%     setActive = abs(betapath(:,k))>1e-16;
+%     betapath(~setActive,k) = 0;
+%     setIneqBorder = abs(residIneq)<1e-16;
+%     residIneq(setIneqBorder) = 0;
     % what about >=2 variables become zero/nonzero together ???
     if idx<=p && setActive(idx)
-        % an active coefficient hits 0
+        % an active coefficient hits 0, or
         setActive(idx) = false;
     elseif idx<=p && ~setActive(idx)
         % a zero coefficient becomes nonzero
         setActive(idx) = true;
-    elseif idx>p && setIneqBorder(idx-p)
-        % an ineq on boundary becomes strict
-        setIneqBorder(idx-p) = false;
-    elseif idx>p && ~setIneqBorder(idx-p)
+    elseif idx>p
+        % an ineq on boundary becomes strict, or
         % a strict ineq hits boundary
-        setIneqBorder(idx-p) = true;
+        setIneqBorder(idx-p) = ~setIneqBorder(idx-p);
     end
+    nActive = nnz(setActive);
 
 end
+
+% clean up
+betapath(:, k:end) = [];
+dualpathEq(:, k:end) = [];
+dualpathIneq(:, k:end) = [];
+rhopath(k:end) = [];
 
 end
